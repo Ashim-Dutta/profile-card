2d23abb71e1ded511fefe9d5bdd59865580db7f7
Ashim-Dutta <ashimdutta800@gmail.com>
2023-12-07 10:36:24 +0530

    This is use for people profile visiting

---
+++ src/data.js
@@ -0,0 +1,60 @@
+const reviews = [
+
+    {
+
+        id:1,
+        name:"Ashim Dutta",
+        job:"Software Developer",
+        image:"https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_640.jpg",
+        text:"The sun dipped below the horizon, casting a warm glow on the tranquil lake. Birds returned to their nests, and the world embraced the calm of twilight. In this serene moment, nature whispered its timeless secrets, inviting reflection. A gentle breeze carried the promise of a peaceful night under the stars."
+
+
+    },
+
+    {
+
+        id:2,
+        name:"Aman Verma",
+        job:"Software Developer",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE5fPhctwNLodS9VmAniEw_UiLWHgKs0fs1w&usqp=CAU",
+        text:"The sun dipped below the horizon, casting a warm glow on the tranquil lake. Birds returned to their nests, and the world embraced the calm of twilight. In this serene moment, nature whispered its timeless secrets, inviting reflection. A gentle breeze carried the promise of a peaceful night under the stars."
+
+
+    },
+
+    {
+
+        id:3,
+        name:"Anupam Yadav",
+        job:"Software Developer",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQai3tIfJimnRynfvYG79pLkzu87CcPsRa6gEkYhbnUdLJXme6c9H7c9l98NgQuTSW4U94&usqp=CAU",
+        text:"The sun dipped below the horizon, casting a warm glow on the tranquil lake. Birds returned to their nests, and the world embraced the calm of twilight. In this serene moment, nature whispered its timeless secrets, inviting reflection. A gentle breeze carried the promise of a peaceful night under the stars."
+
+
+    },
+
+    {
+
+        id:4,
+        name:"Pritish Samajpati",
+        job:"Software Developer",
+        image:"https://cdn130.picsart.com/284258921054201.jpg",
+        text:"The sun dipped below the horizon, casting a warm glow on the tranquil lake. Birds returned to their nests, and the world embraced the calm of twilight. In this serene moment, nature whispered its timeless secrets, inviting reflection. A gentle breeze carried the promise of a peaceful night under the stars."
+
+
+    },
+
+    {
+
+        id:5,
+        name:"Sanidhya",
+        job:"Software Developer",
+        image:"https://petalbackfarm.com/wp-content/uploads/2022/12/heirloom-flowers-after-petal-back-farm-819x1024.jpg",
+        text:"The sun dipped below the horizon, casting a warm glow on the tranquil lake. Birds returned to their nests, and the world embraced the calm of twilight. In this serene moment, nature whispered its timeless secrets, inviting reflection. A gentle breeze carried the promise of a peaceful night under the stars."
+
+
+    },
+
+];
+
+export default reviews;
